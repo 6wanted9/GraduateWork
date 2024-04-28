6140f092a03a0817b2785051ad9513c62c98cd0b
6wanted9 <dkatanov2002@gmail.com>
2024-04-28 19:39:28 +0300

    Updated gitignore

--- GraduateWorkApi/GraduateWork.Api/Controllers/AuthController.cs
+++ GraduateWorkApi/GraduateWork.Api/Controllers/AuthController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace GraduateWorkApi.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class AuthController : ControllerBase
 {
     private readonly IAuthenticationService _authenticationService;
